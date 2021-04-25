f8a9ba76e71a92ababc20532c785fa484a1bf3d1
Jose Bolanos <bolanosjjr@gmail.com>
2021-04-25 13:40:36 -0500

    zero/frquency done

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,10 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === 0)
+        count++;
+    }
+    return count;
 }
 
 module.exports = countZeroes
