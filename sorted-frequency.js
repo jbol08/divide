0ccc87f487259f747e5c11dbed08b5cba5ba2259
Jose Bolanos <bolanosjjr@gmail.com>
2021-04-25 13:48:22 -0500

    sorted freq done

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,38 +1,3 @@
-// function sortedFrequency(arr, val) {
-//     let firstFound = findFirst(arr,val)
-//     if (firstFound === -1) return firstFound;
-//     let lastFound = findLast(arr, val);
-//     return lastFound - firstFound + 1;
-      
-// }
-// function findFirst(arr, val, min = 0, max = arr.length - 1) {
-     
-//     if (max >= min) {
-//     let mid = Math.floor((min + max) / 2)
-//     if ((mid === 0 || val > arr[mid - 1]) && arr[mid] === val) {
-//       return mid;
-//     } else if (val > arr[mid] ) {
-//       return findFirst(arr,val, mid + 1, max)
-//     }
-//     return findFirst(arr, val, min, mid - 1)
-//   }
-//     return -1;
-    
-// }
-// function findLast(arr, val, min = 0, max = arr.length - 1) {
-     
-//     if (max >= min) {
-//     let mid = Math.floor((min + max) / 2)
-//     if ((mid === arr.length - 1 || val < arr[mid + 1]) && arr[mid] === val) {
-//       return mid;
-//     } else if (val < arr[mid]) {
-//       return findLast(arr,val, mid - 1, min)
-//     }
-//     return findLast(arr, val, mid + 1, max)
-//   }
-//     return -1;
-    
-// }
 function sortedFrequency(arr, val) {
     let firstFound = findFirst(arr, val);
     if (firstFound == -1) return firstFound;
@@ -43,9 +8,9 @@ function sortedFrequency(arr, val) {
   function findFirst(arr, val, min = 0, max = arr.length - 1) {
     if (max >= min) {
       let mid = Math.floor((min + max) / 2)
-      if ((mid === 0 || val > arr[mid - 1]) && arr[mid] === val) {
+      if ((mid === 0 ||  arr[mid - 1] < val) && arr[mid] === val) {
         return mid;
-      } else if (val > arr[mid]) {
+      } else if ( arr[mid] < val) {
         return findFirst(arr, val, mid + 1, max)
       } else {
         return findFirst(arr, val, min, mid - 1)
@@ -57,9 +22,9 @@ function sortedFrequency(arr, val) {
   function findLast(arr, val, min = 0, max = arr.length - 1) {
     if (max >= min) {
       let mid = Math.floor((min + max) / 2)
-      if ((mid === arr.length - 1 || val < arr[mid + 1]) && arr[mid] === val) {
+      if ((mid === arr.length - 1 ||  arr[mid + 1] > val) && arr[mid] === val) {
         return mid;
-      } else if (val < arr[mid]) {
+      } else if (arr[mid] > val) {
         return findLast(arr, val, min, mid - 1)
       } else {
         return findLast(arr, val, mid + 1, max)
